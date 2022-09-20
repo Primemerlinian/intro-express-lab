18ec9e3a4ccff79aa61a5e35379ced1ee42b889f
Primemerlinian <mwatson0518@gmail.com>
2022-09-20 17:55:33 -0400

    init github

---
+++ data/students-data.js
@@ -0,0 +1,22 @@
+const students = [
+  {text: 'Elo Floriane', here: true, _id: 125223},
+  {text: 'Abe Kruno', here: false, _id: 127904},
+  {text: 'Berengaria Aisha', here: true, _id: 139906 },
+  {text: 'Theodoard Adolfito', here: true, _id:139907 },
+  {text: 'Merida Hortensia', here: true, _id:139908 },
+  {text: 'Longina Farrah', here: false, _id:139909 },
+  {text: 'Sekhar Æthelwine', here: true, _id:139910 },
+  {text: 'Homeros Viviana', here: true, _id:139911 },
+  {text: 'Ilonka Dehgewanus', here: true, _id:139912 },
+  {text: 'Archembald Shiva', here: false, _id:139913 },
+  {text: 'Shayma Gayan', here: false, _id:139914 },
+  {text: 'Ali Alina', here: true, _id:139915 },
+  {text: 'Galeazzo Damjana', here: false, _id:139916 },
+  {text: 'Mileva Æþelbeorht', here: true, _id:139917 },
+  {text: 'Alva Umid', here: false, _id:139918 },
+  {text: 'Cher Gus', here: true, _id:139919 },
+]
+
+export { 
+	students
+}
